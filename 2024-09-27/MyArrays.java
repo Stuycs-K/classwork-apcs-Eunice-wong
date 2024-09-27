44cb9d03503d38ef8a82ec0d192c6957af571bed
Eunice Wong <eunicew4@nycstudents.net>
2024-09-27 10:02:30 -0400

    cw

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,20 @@
+public class MyArrays.java{
+  public static String aryToString(int[] nums){
+    String result = "";
+    for (int i = 0; i < nums.length; i++){
+      if (i == nums.length - 1){
+        result = result + nums[i];
+      }
+      result = result + nums[i] + ",";
+    }
+    return = "[" + result + "]";
+  }
+  public static int[] returnCopy(int[]ary){
+    int copy = 0;
+
+  }
+  public static int[] concatArray(int[]ary1, int[]ary2){
+
+    }
+  }
+}
