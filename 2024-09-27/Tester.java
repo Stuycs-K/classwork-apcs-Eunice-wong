9bfad0af5b04e89aaedaf13bcbe03a44c06ec08b
Eunice Wong <eunicew4@nycstudents.net>
2024-09-28 23:20:33 -0400

    hw

--- 2024-09-27/Tester.java
+++ 2024-09-27/Tester.java
@@ -6,5 +6,6 @@ public class Tester{
        System.out.println("Expected [-5, 90, 9] vs my function:" + MyArrays.aryToString(b));
        System.out.println("Expected " + MyArrays.aryToString(a) + " vs my function:" + MyArrays.aryToString(MyArrays.returnCopy(a)));
        System.out.println("Expected " + MyArrays.aryToString(b) + " vs my function:" + MyArrays.aryToString(MyArrays.returnCopy(b)));
+       System.out.println("Expected [4, 5, 6,7, -5, 90, 9] vs my function:" + MyArrays.concatArray(a, b) )
     }
 }
