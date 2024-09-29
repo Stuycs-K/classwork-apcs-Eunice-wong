c050ce30e8591828def19df02ef41856ca0c6ef5
Eunice Wong <eunicew4@nycstudents.net>
2024-09-28 22:57:22 -0400

    hw

---
+++ 2024-09-27/Tester.java
@@ -0,0 +1,10 @@
+public class Tester{
+    public static void main(String[] args){
+       int[] a = {4,5,6,7};
+       int[] b = {-5, 90, 9};
+       System.out.println("Expected [4, 5, 6, 7] vs my function:" + MyArrays.aryToString(a));
+       System.out.println("Expected [-5, 90, 9] vs my function:" + MyArrays.aryToString(b));
+       System.out.println("Expected " + MyArrays.aryToString(a) + " vs my function:" + MyArrays.aryToString(MyArrays.returnCopy(a)));
+       System.out.println("Expected " + MyArrays.aryToString(b) + " vs my function:" + MyArrays.aryToString(MyArrays.returnCopy(b)));
+    }
+}
