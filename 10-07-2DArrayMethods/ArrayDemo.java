bae28fc3ddc1d64aec6e978d609620725f6b5cd7
Eunice Wong <eunicew4@nycstudents.net>
2024-10-07 10:15:15 -0400

    cw

--- 10-07-2DArrayMethods/ArrayDemo.java
+++ 10-07-2DArrayMethods/ArrayDemo.java
@@ -1,6 +1,22 @@
 public class ArrayDemo{
   public static void main(String[] args){
-    return 1;
+    int[] test1d = new int[]{1, 2, 3, 4};
+    int[][] test2d = new int[4][2];
+    System.out.println("expected:" + Arrays.toString(test1d) + "\nresult: " + aryToString(test1d));
+    System.out.println("expected:" + Arrays.toString(test2d) "\nresult: " + arrToString(test2d));
+    int[][] tester = new int[][] {{2, 4, 5}, {3, 4, 9}, {9, 19, 21}};
+    int[][] empty = new int[4][2];
+    System.out.println("expected: 76\nresult: " + arr2DSum(tester));
+    System.out.println("expected: 0\nresult: " + arr2DSum(empty));
+    int[][] tester1 = new int[][] {{2, 3, 4}, {6, 7, 8}};
+    System.out.println("expected: [[2, 6], [3, 7], [4, 8]]\nresult: " + arrToString(swapRC(tester1)));
+    int[][] negatives = new int[][] {{-9, 3, -8}, {0, -3, -4, 1}};
+    replaceNegative(negatives);
+    System.out.println("expected: [[1, 3, 0], [0, 1, 0, 1]]\nresult: " + arrToString(negatives));
+    int[][] original = new int[][] {{2, 3, 4,}, {5, 6, 7}};
+    int[][] dup = copy(original);
+    original[0][0] = 99;
+    System.out.println("expected: [[2, 3, 4], [5, 6, 7]]\nresult: " + arrToString(copy(dup)));
   }
   public static String aryToString(int[] nums) {
     String result = "[";
