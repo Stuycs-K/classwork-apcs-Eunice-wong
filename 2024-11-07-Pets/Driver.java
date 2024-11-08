4d115a23e990b68695a154e3b2a8f1650a647a0e
Eunice Wong <eunicew4@nycstudents.net>
2024-11-07 23:49:04 -0500

    added test cases and finished bird class

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,4 +1,13 @@
-public class Animal {
+//Instead of just printing out the original speak() method, it adds on the string "The Mighty" in front of it to create the string "The mighty NAME"
+public class Driver {
+  public static void main(String[] args){
+    Animal Animal1 = new Animal("arg", 15, "Sa");
+    Animal1.speak();
+    Bird Bird1 = new Bird("arg", 15, "Sa", 12.3, "red");
+    Bird1.speak();
+  }
+}
+class Animal {
   private String noise;
   private int age;
   private String name;
@@ -11,7 +20,7 @@ public class Animal {
   public void speak(){
     System.out.println("My name is " + getName());
     System.out.println("I am " + getAge() + " years old" );
-    System.out.println("I say " + getnoise());
+    System.out.println("I say " + getNoise());
   }
 
   public String getName(){
@@ -24,42 +33,29 @@ public class Animal {
     return noise;
   }
 }
-public class Bird extends Animal {
+class Bird extends Animal {
     private double height;
     private String color;
 
 public Bird(String noise, int age, String name, double height, String color){
-    //In order to construct the Bird you must first call the superclass constructor
-    //This is done by saying: super(parameters)
-    //e.g. super("Doug",10,"Squaaaak!");
-    super(String noise, int age, String name)
+    super(noise, age, name);
+    this.height = height;
+    this.color = color;
   }
-
-
-  //6.Override the speak() method.
-  /*WITHOUT REPEATING CODE:
-  print the following message replacing the ? with name/age/noise/color/height:
-    My name is ?.
-    I am ? years old.
-    I say ?.
-    My feathers are colored ?
-    I am ? inches tall
-  */
-  //You can call the old method using super.speak();
   public void speak(){
     super.speak();
     System.out.println("My feathers are colored "+ color);
     System.out.println("I am " + height + " inches tall");
   }
-
-  //7:Override the getName() method so that a bid's name is "The Mighty NAME" instead of "NAME"
-  //e.g. "The Mighty Polly" instead of just "Polly"
-  //You can call the superclass' getName method using the syntax: super.getName()
-
+  public String getName(){
+    return "The Mighty " + super.getName();
+    }
+    
   //Step 8:In Driver.java answer: What is the difference before and after you override this method?
   //What does this tell us about methods that are overridden?
   //public String getName(){  }
 
   //Go back and complete steps 9 onwards.
-
 }
+
+
