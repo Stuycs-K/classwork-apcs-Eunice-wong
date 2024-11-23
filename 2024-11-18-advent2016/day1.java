d9624f9c8fb3cd7b87b6a6cf00da02e81b6a5e04
Eunice Wong <eunicew4@nycstudents.net>
2024-11-23 18:32:16 -0500

    altered day 1 to fit part 2

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class day1{
     public static int countDistance(){
         int x = 0;
@@ -5,6 +6,8 @@ public class day1{
         int position = 1;
         String input = "R3, R1, R4, L4, R3, R1, R1, L3, L5, L5, L3, R1, R4, L2, L1, R3, L3, R2, R1, R1, L5, L2, L1, R2, L4, R1, L2, L4, R2, R2, L2, L4, L3, R1, R4, R3, L1, R1, L5, R4, L2, R185, L2, R4, R49, L3, L4, R5, R1, R1, L1, L1, R2, L1, L4, R4, R5, R4, L3, L5, R1, R71, L1, R1, R186, L5, L2, R5, R4, R1, L5, L2, R3, R2, R5, R5, R4, R1, R4, R2, L1, R4, L1, L4, L5, L4, R4, R5, R1, L2, L4, L1, L5, L3, L5, R2, L5, R4, L4, R3, R3, R1, R4, L1, L2, R2, L1, R4, R2, R2, R5, R2, R5, L1, R1, L4, R5, R4, R2, R4, L5, R3, R2, R5, R3, L3, L5, L4, L3, L2, L2, R3, R2, L1, L1, L5, R1, L3, R3, R4, R5, L3, L5, R1, L3, L5, L5, L2, R1, L3, L1, L3, R4, L1, R3, L2, L2, R3, R3, R4, R4, R1, L4, R1, L5";
         String[] arr = input.split(", ");
+        ArrayList<String> visited = new ArrayList<>();
+        visited.add(x + "," + y);
         for (int i = 0; i < arr.length; i++){
             String move = arr[i];
             char direction = move.charAt(0);
@@ -17,7 +20,7 @@ public class day1{
             } else if (direction == 'R') {
                 position = (position + 1) % 4;
             }
-
+        for (int i = 0; i < distance, i++){
             if (position == 1){
                 y += distance;
             }
@@ -30,9 +33,14 @@ public class day1{
             else {
                 x -= distance;
             }
-
+            String currentLocation = x + "," + y;
+            if (visitied.contains(currentLocation)){
+                return Math.abs(x) + Math.abs(y);
+            }
+        visited.add(currentLocation);
         }
-        return Math.abs(x) + Math.abs(y);
+        }
+        return -1;
     }
     public static void main(String[] args) {
         System.out.println("Manhattan Distance: " + countDistance());
