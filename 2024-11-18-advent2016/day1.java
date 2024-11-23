b9ef6ceb13191e5f659fd8a087dec31157267ac6
Eunice Wong <eunicew4@nycstudents.net>
2024-11-23 15:09:24 -0500

    finished day 1

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -1,35 +1,22 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class day1{
-    public static int countDistance(String filename){
+    public static int countDistance(){
         int x = 0;
         int y = 0;
         int position = 1;
-        try{
-        File file = new File(filename);
-        Scanner input = new Scanner(file);
-        while (input.hasNext()) {
-            String direction = input.next(); // "L" or "R"
-            if (!input.hasNextInt()) {
-                    throw new NumberFormatException("Expected a number after " + direction);
-                }
-            int distance = Integer.parseInt(input.next());; // Distance value
-
-            if (position > 4){
-                position = 1;
+        String input = "R3, R1, R4, L4, R3, R1, R1, L3, L5, L5, L3, R1, R4, L2, L1, R3, L3, R2, R1, R1, L5, L2, L1, R2, L4, R1, L2, L4, R2, R2, L2, L4, L3, R1, R4, R3, L1, R1, L5, R4, L2, R185, L2, R4, R49, L3, L4, R5, R1, R1, L1, L1, R2, L1, L4, R4, R5, R4, L3, L5, R1, R71, L1, R1, R186, L5, L2, R5, R4, R1, L5, L2, R3, R2, R5, R5, R4, R1, R4, R2, L1, R4, L1, L4, L5, L4, R4, R5, R1, L2, L4, L1, L5, L3, L5, R2, L5, R4, L4, R3, R3, R1, R4, L1, L2, R2, L1, R4, R2, R2, R5, R2, R5, L1, R1, L4, R5, R4, R2, R4, L5, R3, R2, R5, R3, L3, L5, L4, L3, L2, L2, R3, R2, L1, L1, L5, R1, L3, R3, R4, R5, L3, L5, R1, L3, L5, L5, L2, R1, L3, L1, L3, R4, L1, R3, L2, L2, R3, R3, R4, R4, R1, L4, R1, L5";
+        String[] arr = input.split(", ");
+        for (int i = 0; i < arr.length; i++){
+            String move = arr[i];
+            char direction = move.charAt(0);
+            int distance = Integer.parseInt(move.substring(1));
+            if (position == 0){
+                position = 4;
             }
-
-            if (direction.equals("L")){
-                position --;
+            if (direction == 'L') {
+                position = (position + 3) % 4; 
+            } else if (direction == 'R') {
+                position = (position + 1) % 4;
             }
-            else if(direction.equals("R")){
-                position ++;
-            }
-            else {
-                    throw new IllegalArgumentException("Unexpected direction: " + direction);
-                }
 
             if (position == 1){
                 y += distance;
@@ -45,17 +32,9 @@ public class day1{
             }
 
         }
-        input.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + filename);
-            return 0;
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid number format in file");
-            return 0;
-        }
         return Math.abs(x) + Math.abs(y);
     }
     public static void main(String[] args) {
-        System.out.println(countDistance("day1.txt"));
+        System.out.println("Manhattan Distance: " + countDistance());
     }
 }
