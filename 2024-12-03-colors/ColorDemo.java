6fac38790908af83e7ddb5a26514a7cbb27a424f
Eunice Wong <eunicew4@nycstudents.net>
2024-12-03 22:19:59 -0500

    added rain drops

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,4 +1,20 @@
 public class ColorDemo{
+    public static final String CLEAR_SCREEN =  "\u001b[2J";
+    public static final String HIDE_CURSOR =  "\u001b[?25l";
+    public static final String SHOW_CURSOR =  "\u001b[?25h";
+    public static int BLACK = 30;
+    public static int RED = 31;
+
+    public static void color(int foreground,int background){
+        System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+    }
+
+    public static Void color(int foreground, int background, int modifier){
+        System.out.print( "\u001b[" + foreground + ";" + (10+background) + + ";" + modifier + "m");
+    }
+    public static void go(int r,int c){
+        System.out.print("\u001b[" + r + ";" + c + "f");
+    }
     public static void main(String[] args){
         /*
         for(int r = 0; r < 256; r+=32){
@@ -10,5 +26,19 @@ public class ColorDemo{
             System.out.println();
         }
         */
+        System.out.print(CLEAR_SCREEN + HIDE_CURSOR);
+        int rows = 25;
+        int col = 25;
+        int drops = 75;
+        for(int i = 0; i < drops; i++){
+            int randomCol = (int) (Math.random() * col) + 1;
+            for (int r = 0; r <= rows; r++){
+                go(r, randomCol);
+                System.out.println(",");
+                go(r, randomCol);
+                System.out.priny(" ");
+            }
+        }
+        System.out.print(SHOW_CURSOR);
     }
 }
