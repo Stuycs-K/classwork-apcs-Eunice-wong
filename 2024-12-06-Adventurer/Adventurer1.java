40274e8b2d44db6a3550b0867c2ebf5bdeda4dd6
Eunice Wong <eunicew4@nycstudents.net>
2024-12-08 17:23:46 -0500

    wrote constructors

--- 2024-12-06-Adventurer/Adventurer1.java
+++ 2024-12-06-Adventurer/Adventurer1.java
@@ -1,3 +1,14 @@
 public class Adventurer1 extends Adventurer{
-  
+    private int fireDamage;
+    private int fireDamageMax;
+    
+    public Warrior(String name){
+        this(name, 20, 100);
+    }
+    public Warrior(String name, int hp, int fireDamageMax){
+        super(name, hp);
+        this.fireDamageMax = fireDamageMax;
+        this.fireDamage = 0;
+    }
+            
 }
