ea7407755e39a901b08ebdcbf0fa8be02fe815c6
Eunice Wong <eunicew4@nycstudents.net>
2024-12-08 17:30:10 -0500

    wrote personal Specials

--- 2024-12-06-Adventurer/Adventurer1.java
+++ 2024-12-06-Adventurer/Adventurer1.java
@@ -10,5 +10,40 @@ public class Adventurer1 extends Adventurer{
         this.fireDamageMax = fireDamageMax;
         this.fireDamage = 0;
     }
-            
+    public abstract String getSpecialName(){
+        return "Fire";
+    }
+    public abstract int getSpecial(){
+        return fireDamage;
+    }
+    public abstract void setSpecial(int n){
+        if (n < 0){
+            fireDamage = 0;
+        }
+        else if (n > fireDamageMax){
+            fireDamage = fireDamageMax;
+        }
+        else {
+        fireDamage = n;
+        }
+    }
+    public abstract int getSpecialMax(){
+        return fireDamageMax;
+    }
+
+  /*
+    all adventurers must have a way to attack enemies and
+    support their allys
+  */
+  //hurt or hinder the target adventurer
+  public abstract String attack(Adventurer other);
+
+  //heall or buff the target adventurer
+  public abstract String support(Adventurer other);
+
+  //heall or buff self
+  public abstract String support();
+
+  //hurt or hinder the target adventurer, consume some special resource
+  public abstract String specialAttack(Adventurer other);
 }
