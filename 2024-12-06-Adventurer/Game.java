78c416241e2bc54c82ab8ef7066c7c8171cd4d22
Eunice Wong <eunicew4@nycstudents.net>
2024-12-09 21:23:05 -0500

    wrote names and print statement if one of them dies

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
-public class Game extends Adventurer{
+import java.util.Random;
+public class Game{
   public static void main(String[] args) {
-    Adventurer p1 = new Adventurer1();
-    Adventurer p2 = new CodeWarrior();
+    Adventurer p1 = new Adventurer1("player");
+    Adventurer p2 = new CodeWarrior("enemy");
     Scanner userInput = new Scanner(System.in);
-    try{
-    System.out.println("Enter Adventurer username");
-    Adventurer1.setname(userInputnextLine());
-    System.out.println("Enter CodeWarrior username");
-    CodeWarrior.setname(userInputnextLine());
+    boolean gameRunning = true;
+    while(gameRunning){
+    System.out.println(p1.getName() + ", " + p1.getHP() + "/" + p1.getmaxHP() + ", " + p1.getSpecial() + "/" + p1.getSpecialMax() + ", " + p1.getSpecialName());
+    System.out.println(p2.getName() + ", " + p2.getHP() + "/" + p2.getmaxHP() + ", " + p2.getSpecial() + "/" + p2.getSpecialMax() + ", " + p2.getSpecialName());
+    if(p1.getHP() <= 0){
+      System.out.println("Slained by enemy! Play again.");
+      gameRunning = false;
+    }
+    if(p2.getHP() <=0){
+      System.out.println("You won!");
+      gameRunning = false;
+    }
+    String action = "";
+    boolean invalid = false;
+    while (!invalid){
+      
+    }
 }
