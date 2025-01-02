9ffd3b28ed2b23e94273dc86c570edf8aa461ffa
Eunice Wong <eunicew4@nycstudents.net>
2025-01-02 10:14:52 -0500

    fib

---
+++ 2025-01-02-Fibonacci/Fibonacci.java
@@ -0,0 +1,11 @@
+public class Fibonacci{
+  public static int fib(int n){
+    if (n > 1){
+      return fib(n-1) + fib(n-2);
+    }
+    return n;
+  }
+  public static void main(String[] args){
+    
+  }
+}
